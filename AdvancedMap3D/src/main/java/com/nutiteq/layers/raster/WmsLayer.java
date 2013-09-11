4817ef9a0d13511c2a0b1f05bda6ecfef2e960f5
Jaak Laineste <jaak@nutiteq.com>
2013-09-11 09:27:36 +0300

    import cleanup

--- AdvancedMap3D/src/main/java/com/nutiteq/layers/raster/WmsLayer.java
+++ AdvancedMap3D/src/main/java/com/nutiteq/layers/raster/WmsLayer.java
@@ -1,24 +1,6 @@
 package com.nutiteq.layers.raster;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Map;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.net.http.AndroidHttpClient;
 
 import com.nutiteq.components.Envelope;
 import com.nutiteq.components.MapPos;
@@ -39,9 +21,6 @@ import com.nutiteq.utils.Utils;
  * @author jaak
  */
 public class WmsLayer extends RasterLayer {
-
-
-
 
     private String layer;
     private String format;
